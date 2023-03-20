87373e7302f8af88d62606f48bc83aa5840c8980
Manas <singhmanas610@gmail.com>
2023-03-20 19:06:22 +0530

    Created react app and a component

---
+++ reactapp/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import TaskItem from './Components/TaskItem';
+
+
+function App() {
+  return (
+    //write component name starting with capital letter
+    <div>
+      <h2>React app just for practice</h2>
+      <TaskItem></TaskItem>
+    </div>
+  );
+}
+
+export default App;
---
+++ reactapp/src/Components/TaskItem.js
@@ -0,0 +1,5 @@
+function TaskItem(){
+    return <h2>Componenet Name should start from Capital</h2>
+}
+
+export default TaskItem;
