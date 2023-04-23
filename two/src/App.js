3d446a13729c747e4e512d5683707dd070cf629e
Manas <singhmanas610@gmail.com>
2023-04-23 14:53:52 +0530

    Initial react app with chakra ui

---
+++ two/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <Router>
+    <Routes>
+      <Route />
+    </Routes>
+    </Router>
+  );
+}
+
+export default App;
