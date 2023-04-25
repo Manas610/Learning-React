8c68357525ecfd0fb9784f33d608b9bd70534029
Manas <singhmanas610@gmail.com>
2023-04-26 01:24:36 +0530

    coin and exchange cards

--- three/src/Coins.jsx
+++ three/src/Coins.jsx
@@ -1,11 +1,67 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { server } from ".";
+import {
+  Container,
+  HStack,
+  VStack,
+  Image,
+  Heading,
+  Text,
+} from "@chakra-ui/react";
+import Loader from "./Loader";
+import ErrorComponent from "./ErrorComponent";
+import CoinCard from "./CoinCard";
 
 const Coins = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [page, setPage] = useState(1);
+  const [currency, setCurrency] = useState("inr");
 
-export default Coins
+  const currencySymbol = currency==="inr"?"₹": currency==="eur"?"€" : "$" 
+
+  useEffect(() => {
+    const fetchCoins = async () => {
+      try {
+        const { data } = await axios.get(`${server}/coins/markets?vs_currency=${currency}&page=${page}`);
+        setCoins(data);
+        setLoading(false);
+      } catch (error) {
+        setLoading(false);
+        setError(true);
+      }
+    };
+    fetchCoins();
+  }, [currency,page]);
+
+  if (error) return <ErrorComponent message={"Holy Shit! no fetching of coins done"} />;
+
+  return (
+    <Container maxW={"container.xl"}>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <HStack wrap={"wrap"}>
+            {coins.map((i) => (
+              <CoinCard
+                id={i.id}
+                key={i.id}
+                name={i.name}
+                price={i.current_price}
+                img={i.image}
+                symbol={i.symbol}
+                url={i.url}
+                currencySymbol={currencySymbol}
+              />
+            ))}
+          </HStack>
+        </>
+      )}
+    </Container>
+  );
+};
+
+export default Coins;
---
+++ three/src/Loader.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loader = () => {
+  return (
+    <div>
+      Heeeeeeeeeeeeeeeeeeeeeeeeeehaaaaaaaaaaaaaaaaaaaaa
+    </div>
+  )
+}
+
+export default Loader
