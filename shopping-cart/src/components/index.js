542121dfceba85ae21a28b6108d195b550eadd43
Manas <singhmanas610@gmail.com>
2023-08-12 21:45:36 +0530

    removed crypto-stats

--- shopping-cart/src/components/index.js
+++
@@ -1,3 +0,0 @@
-export { Header } from "./Header"
-export { ProductCard } from "./ProductCard"
-export { CartCard } from "./CartCard"
--- shopping-cart/src/pages/index.js
+++
@@ -1,2 +0,0 @@
-export { Home } from "./Home"
-export { Cart } from "./Cart"
